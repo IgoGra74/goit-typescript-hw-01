9029d67b617ad2da57025ee0f1d3e5e329cc974f
IgorGra1 <gis1@ukr.net>
2024-05-07 18:32:31 +0300

    update

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -1,13 +1,11 @@
-type User = {
+type UserType = {
   name: string;
   surname: string;
   email: string;
   password: string;
 };
 
-function createOrUpdateUser(initialValues: Partial<User>) {
-  // Оновлення користувача
-}
+function createOrUpdateUser<T extends Partial<UserType>>(initialValues: T) {}
 
 createOrUpdateUser({
   email: "user@mail.com",
